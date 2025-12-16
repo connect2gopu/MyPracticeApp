63d954b4d3a581ee70331db399146a28e8479e3a
Gopal Sharma <connect2gopu@gmail.com>
2025-12-16 09:26:35 +0530

    Add UseReducer Page

--- app/(tabs)/testPage.tsx
+++ app/(tabs)/testPage.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useReducer } from 'react';
+import { Button, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 const DummyPage = () => {
@@ -8,7 +8,104 @@ const DummyPage = () => {
     </>
 }
 
-type PageType = "DUMMY";
+type CounterState = number;
+type TodoState = string[]
+
+type CounterAction = { type: "INC" }
+    | { type: "RESET" }
+    | { type: "DEC" };
+type TodoAction = { type: "ADD_TODO", payload: string }
+
+type AppAction = CounterAction | TodoAction;
+
+type RootState = {
+    count: CounterState,
+    todo: TodoState
+}
+
+type Reducer<S, A> = (state: S | undefined, action: A) => S;
+type ReducerMap = {
+    [K in keyof RootState]: Reducer<RootState[K], AppAction>
+}
+
+function combineReducer(
+    reducers: ReducerMap
+): Reducer<RootState, AppAction> {
+    const reducerKeys = Object.keys(reducers) as (keyof RootState)[];
+
+    return function combination(state: RootState | undefined = {} as RootState, action: AppAction): RootState {
+        const nextState = {} as RootState
+
+        for (let i = 0; i < reducerKeys.length; i++) {
+
+            const key = reducerKeys[i]
+            const reducer = reducers[key] as Reducer<RootState[typeof key], AppAction>
+
+            const prevStateForKey = state?.[key] as RootState[typeof key] | undefined
+            const nextStateForKey = reducer(prevStateForKey, action)
+
+                ; (nextState as Record<keyof RootState, CounterState | TodoState>)[key] = nextStateForKey
+        }
+
+        return nextState;
+    }
+}
+
+function counterReducer(
+    state: CounterState | undefined = 0,
+    action: AppAction
+): CounterState {
+    switch (action.type) {
+        case "INC":
+            return (state ?? 0) + 1;
+        case "RESET":
+            return 0
+        case "DEC":
+            return (state ?? 0) - 1;
+        default:
+            return state ?? 0
+    }
+}
+
+function todoReducer(
+    state: TodoState | undefined = [],
+    action: AppAction
+): TodoState {
+    switch (action.type) {
+        case "ADD_TODO":
+            return [...(state ?? []), action.payload]
+        default:
+            return state ?? [];
+    }
+}
+
+const rootReducer = combineReducer({
+    count: counterReducer,
+    todo: todoReducer
+});
+
+const initialState: RootState = {
+    count: 0,
+    todo: []
+}
+
+const UseReducerPage = () => {
+
+    const [state, dispatch] = useReducer(rootReducer, initialState);
+
+    return <>
+        <Text>This is a use reducer</Text>
+        <Text>{`Count: ${state.count}`}</Text>
+
+        <View style={{display: 'flex', flexDirection: 'row', gap: 30, justifyContent: 'space-evenly'}}>
+            <Button title='Increase' onPress={() => dispatch({ type: 'INC'})}/>
+            <Button title='Reset' onPress={() => dispatch({ type: 'RESET'})}/>
+            <Button title='Decrese' onPress={() => dispatch({ type: 'DEC'})}/>
+        </View>
+    </>
+}
+
+type PageType = "DUMMY" | "USE_REDUCER";
 interface PageContentProps {
     pageType: PageType
 }
@@ -20,14 +117,15 @@ const PageContent = ({ pageType }: PageContentProps) => {
 }
 
 const pageMapper: Record<PageType, React.JSX.Element> = {
-    DUMMY: <DummyPage />
+    DUMMY: <DummyPage />,
+    USE_REDUCER: <UseReducerPage />
 }
 
 const TestPage = (): React.JSX.Element => {
     return <SafeAreaView style={{ flex: 1, backgroundColor: "#4A90E2" }}>
         <View style={{ flex: 1, padding: 32, backgroundColor: "#c1eaf5" }}>
-            <PageContent pageType={"DUMMY"} />
-            {/* <PageContent pageType={"USE_REDUCER"} /> */}
+            {/* <PageContent pageType={"DUMMY"} /> */}
+            <PageContent pageType={"USE_REDUCER"} />
         </View>
     </SafeAreaView>
 }
